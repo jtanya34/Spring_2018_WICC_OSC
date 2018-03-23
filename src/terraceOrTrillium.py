1eddd4e1dbe6f7737ca33df4c765dffb35594fea
tanya <jtanya34@gmail.com>
2018-03-23 23:01:55 +0530

    Changed the terraceOrTrillium file.

--- src/terraceOrTrillium.py
+++ src/terraceOrTrillium.py
@@ -3,3 +3,9 @@
 #'Trillium'.
 def terrace_or_trillium(is_wednesday, likes_orange_chicken, likes_pho):
 	pass
+	if(is_wednesday==True and likes_orange_chicken==True) or (likes_pho==True):
+		result=('Terrace')
+	else:
+		result=('Trillium')
+	print(result)
+	return result
